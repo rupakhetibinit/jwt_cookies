545c1ecd688c2b133c9f5a333ab1aaa36f3147db
rupakhetibinit <crusierponder@protonmail.com>
2023-05-24 20:10:43 +0545

    feat: proper error handling using custom error

--- index.ts
+++ index.ts
@@ -1,22 +1,63 @@
 import express, { NextFunction, Request, Response } from 'express';
-import router from './src/routes/userRoute';
+import 'express-async-errors';
+import userRoute from './src/routes/userRoute';
 import cookieParser from 'cookie-parser';
 const app = express();
 import dotenv from 'dotenv';
+import ApplicationError from './src/error';
+import { TokenExpiredError } from 'jsonwebtoken';
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+
+// Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', router);
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-	console.error(err.stack);
-	res.status(500).send('Something broke!');
-});
-app.get('/', async (req, res) => {
-	res.send('test');
-});
 
-app.listen(port, () => {
-	console.log('Express application running on port', port);
-});
+// Routes
+app.use('/api', userRoute);
+
+// Error Handler
+app.use(
+	(
+		err: ApplicationError | Error,
+		_req: Request,
+		res: Response,
+		_next: NextFunction
+	) => {
+		console.error(err.stack);
+		if (err instanceof ApplicationError) {
+			const status = err.status || 500;
+			console.error(status);
+			return res.status(status).send({
+				error: {
+					message: err.message,
+				},
+			});
+		}
+		if (err instanceof TokenExpiredError) {
+			return res.status(401).send({
+				error: {
+					message: 'Token Expired. Log in again',
+				},
+			});
+		}
+		return res.status(400).send({
+			error: {
+				message: err.message,
+			},
+		});
+	}
+);
+
+async function main() {
+	try {
+		app.listen(port, () => {
+			console.log('Express application running on port', port);
+		});
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+main();
---
+++ src/error.ts
@@ -0,0 +1,16 @@
+// ApplicationError.js
+class ApplicationError extends Error {
+	status: number;
+	constructor(message: string, status: number) {
+		super();
+
+		Error.captureStackTrace(this, this.constructor);
+
+		this.name = this.constructor.name;
+
+		this.message = message || 'Something went wrong. Please try again.';
+
+		this.status = status || 500;
+	}
+}
+export default ApplicationError;
