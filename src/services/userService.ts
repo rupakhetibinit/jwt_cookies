7d36296377f34acebe1c2fb83c9aa134d364980d
rupakhetibinit <crusierponder@protonmail.com>
2023-05-24 20:44:18 +0545

    feat: function to login and signup user

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,25 +3,60 @@ import { UserInput } from '../schema/userSchema';
 import { omit } from 'lodash';
 import jwt from 'jsonwebtoken';
 import { config } from '../config';
+import * as argon2 from 'argon2';
+import ApplicationError from '../error';
 export async function createUser(input: UserInput) {
-	try {
-		const user = await prisma.user.create({
-			data: {
-				email: input.email,
-				password: input.password,
-			},
-		});
-		if (!user) throw new Error('user already exists');
-
-		const accessToken = jwt.sign({ email: user.email }, config.privateKey, {
-			expiresIn: '1m',
-		});
-		const refreshToken = jwt.sign({ email: user.email }, config.privateKey, {
-			expiresIn: '30d',
-		});
-
-		return { ...omit(user, 'password'), accessToken, refreshToken };
-	} catch (error: any) {
-		throw new Error(error);
+	const existingUser = await prisma.user.findUnique({
+		where: {
+			email: input.email,
+		},
+	});
+	if (existingUser) {
+		throw new ApplicationError('User already exists', 409);
 	}
+	const hashedPassword = await argon2.hash(input.password);
+
+	const newUser = await prisma.user.create({
+		data: {
+			email: input.email,
+			password: hashedPassword,
+		},
+	});
+
+	const accessToken = jwt.sign({ email: newUser.email }, config.privateKey, {
+		expiresIn: '1m',
+	});
+
+	const refreshToken = jwt.sign({ email: newUser.email }, config.privateKey, {
+		expiresIn: '30d',
+	});
+
+	return { ...omit(existingUser, 'password'), accessToken, refreshToken };
+}
+
+export async function loginUser(input: UserInput) {
+	const user = await prisma.user.findUniqueOrThrow({
+		where: {
+			email: input.email,
+		},
+	});
+	console.log(user);
+	if (!user) throw new ApplicationError("User doesn't exist", 404);
+	console.log(user.password, input.password);
+
+	const verified = await argon2.verify(user.password, input.password);
+
+	if (!verified) {
+		throw new ApplicationError("Passwords don't match", 400);
+	}
+
+	const accessToken = jwt.sign({ email: input.email }, config.privateKey, {
+		expiresIn: '1m',
+	});
+
+	const refreshToken = jwt.sign({ email: input.email }, config.privateKey, {
+		expiresIn: '30d',
+	});
+
+	return { ...omit(user, 'password'), accessToken, refreshToken };
 }
