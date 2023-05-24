895af28322df393ebc5598a831658a4bc5410727
rupakhetibinit <crusierponder@protonmail.com>
2023-05-24 20:11:46 +0545

    fix: proper naming of route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,14 +1,14 @@
 import { Router, Request, Response } from 'express';
 import validate from '../middleware/validate';
 import verifyAuth from '../middleware/verifyAuth';
-const router = Router();
+const userRoute = Router();
 import { createUserHandler } from '../controllers/userController';
 import { userInputSchema } from '../schema/userSchema';
 
-router.get('/me', verifyAuth, (req: Request, res: Response) => {
+userRoute.get('/me', verifyAuth, (req: Request, res: Response) => {
 	return res.json('nooooooooooooooooooooo');
 });
 
-router.post('/signup', validate(userInputSchema), createUserHandler);
+userRoute.post('/signup', validate(userInputSchema), createUserHandler);
 
-export default router;
+export default userRoute;
