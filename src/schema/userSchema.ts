af42b789fec50393bbe23b4657bce4155576cf5a
rupakhetibinit <crusierponder@protonmail.com>
2023-05-25 09:38:29 +0545

    schema: proper zod schema

--- src/schema/userSchema.ts
+++ src/schema/userSchema.ts
@@ -1,8 +1,24 @@
 import { z } from 'zod';
 
 export const userInputSchema = z.object({
-	email: z.string(),
-	password: z.string(),
+	email: z
+		.string({
+			required_error: 'Email is required',
+		})
+		.email('Email should be a valid email')
+		.min(2, 'Email should be at least 2 characters'),
+	password: z
+		.string({
+			required_error: 'Password is required',
+		})
+		.min(6, 'Password should be at least 6 characters')
+		.max(64, 'Password should be at most 64 characters'),
 });
 
 export type UserInput = z.infer<typeof userInputSchema>;
+
+export const newAccessTokenInputSchema = z.object({
+	refreshToken: z.string(),
+});
+
+export type NewAccessTokenInput = z.infer<typeof newAccessTokenInputSchema>;
