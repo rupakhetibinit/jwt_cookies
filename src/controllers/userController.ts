271d9a9323f5b3fe548bf1efcdf689ffce6301f6
rupakhetibinit <crusierponder@protonmail.com>
2023-05-25 09:14:41 +0545

    add: controller for getting new access token

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,9 @@
 import { Response, Request, NextFunction } from 'express';
-import { UserInput } from '../schema/userSchema';
+import { NewAccessTokenInput, UserInput } from '../schema/userSchema';
 import { createUser, loginUser } from '../services/userService';
+import { config } from '../config';
+import ApplicationError from '../error';
+import jwt from 'jsonwebtoken';
 
 export async function createUserHandler(
 	req: Request<{}, {}, UserInput>,
@@ -24,6 +27,33 @@ export async function signInHandler(
 	try {
 		const user = await loginUser(req.body);
 		return res.send(user);
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function newAcessTokenHandler(
+	req: Request<{}, {}, NewAccessTokenInput>,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		const refreshToken = req.body.refreshToken;
+		let accessToken;
+		const decoded = jwt.verify(refreshToken, config.privateKey) as {
+			email: string;
+		};
+		if (decoded) {
+			accessToken = jwt.sign({ email: decoded.email }, config.privateKey, {
+				expiresIn: '1m',
+			});
+
+			return res.json({
+				refreshToken,
+				accessToken,
+			});
+		}
+		throw new ApplicationError('Something went wrong', 400);
 	} catch (error) {
 		next(error);
 	}
